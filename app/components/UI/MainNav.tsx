9236de20408fa3d440a04fba40123003562d823e
diceboi <122260041+diceboi@users.noreply.github.com>
2023-12-07 05:39:54 +0100

    first try

---
+++ app/components/UI/MainNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function MainNav() {
+  return (
+    <nav className='flex justify-start items-center w-full h-16 p-4 fixed top-0 border-b border-white border-opacity-25 backdrop-blur-xl'>
+        <div className='w-fit h-full'>
+            <Image src="/exvitermini-logo-white.svg" height={75} width={200} alt='exvitermini logo' />
+        </div>
+    </nav>
+  )
+}
