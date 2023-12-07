782fbff026984ad542b4aaf48ef610a509696ee2
diceboi <122260041+diceboi@users.noreply.github.com>
2023-12-07 06:06:23 +0100

    videochanges

--- app/components/UI/MainNav.tsx
+++ app/components/UI/MainNav.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function MainNav() {
   return (
-    <nav className='flex justify-start items-center w-full h-16 p-4 fixed top-0 border-b border-white border-opacity-25 backdrop-blur-xl'>
+    <nav className='flex justify-start items-center w-full h-16 p-4 fixed top-0 border-b border-white border-opacity-25 backdrop-blur-xl z-50'>
         <div className='w-fit h-full'>
             <Image src="/exvitermini-logo-white.svg" height={75} width={200} alt='exvitermini logo' />
         </div>
