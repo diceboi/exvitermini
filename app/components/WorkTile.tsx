48524f21a8ef75c5779a4fa3c4ebe68b4eece1f1
diceboi <122260041+diceboi@users.noreply.github.com>
2024-04-27 22:53:18 +0200

    try cols

--- app/components/WorkTile.tsx
+++ app/components/WorkTile.tsx
@@ -6,8 +6,12 @@ import Link from "next/link";
 
 export default function WorkTile({ id, title, subtitle, cols, slug, images }:any) {
   return (
-        <motion.div className={`relative col-span-${cols} flex flex-col justify-end p-8 border border-white h-[60vh] w-full rounded-3xl transition-all`}
-        whileInView={{ opacity: 1 }} initial={{opacity: 0}} exit={{opacity: 0}}>
+        <motion.div 
+            whileInView={{ opacity: 1 }} 
+            initial={{opacity: 0}} 
+            exit={{opacity: 0}} 
+            className={`relative ${cols === '1' ? 'col-span-1' : `col-span-${cols}`} flex flex-col justify-end p-8 border border-white h-[60vh] w-full rounded-3xl transition-all`}
+        >
             <Link href={`/${slug}`}>
                 <h5 className="text-white text-syne font-regular text-xl opacity-50">{subtitle}</h5>
                 <h2 className="text-white text-syne font-bold text-3xl">{title}</h2>
