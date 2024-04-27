c373a9450a59ac3193fecb37bc157bfd6af5f744
diceboi <122260041+diceboi@users.noreply.github.com>
2024-04-27 23:01:13 +0200

    colspan fuckoff

--- app/components/WorkTile.tsx
+++ app/components/WorkTile.tsx
@@ -10,13 +10,13 @@ export default function WorkTile({ id, title, subtitle, cols, slug, images }:any
             whileInView={{ opacity: 1 }} 
             initial={{opacity: 0}} 
             exit={{opacity: 0}} 
-            className={`relative ${cols === '1' ? 'col-span-1' : `col-span-${cols}`} flex flex-col justify-end p-8 border border-white h-[60vh] w-full rounded-3xl transition-all`}
+            className={`relative col-span-${cols} flex flex-col justify-end p-8 border border-white h-[60vh] w-full rounded-3xl transition-all`}
         >
-            <Link href={`/${slug}`}>
-                <h5 className="text-white text-syne font-regular text-xl opacity-50">{subtitle}</h5>
-                <h2 className="text-white text-syne font-bold text-3xl">{title}</h2>
-                <Image src={images[0]} fill style={{objectFit:"cover"}} alt="alt" className="rounded-3xl opacity-50 hover:opacity-70" />
-            </Link> 
+                <Link href={`/${slug}`}>
+                    <h5 className="text-white text-syne font-regular text-xl opacity-50">{subtitle}</h5>
+                    <h2 className="text-white text-syne font-bold text-3xl">{title}</h2>
+                    <Image src={images[0]} fill style={{objectFit:"cover"}} alt="alt" className="rounded-3xl opacity-50 hover:opacity-70" />
+                </Link> 
         </motion.div>
   )
 }
